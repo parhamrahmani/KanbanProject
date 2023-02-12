c9d9d74863642acd7d41a8c0b21d243d1108b361
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-12 16:21:36 +0100

    Initial Commit

--- src/main/java/KanbanProject/KanbanProjectApplication.java
+++ src/main/java/KanbanProject/KanbanProjectApplication.java
@@ -2,7 +2,7 @@ package KanbanProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//initial commit
 @SpringBootApplication
 public class KanbanProjectApplication {
 
