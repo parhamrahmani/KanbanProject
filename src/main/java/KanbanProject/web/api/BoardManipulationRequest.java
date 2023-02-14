4dc3422f99d5eea34891ac9dfa50ed05718d6185
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-14 21:08:50 +0100

    Manipulation Request Classes implementation

--- src/main/java/KanbanProject/web/api/BoardManipulationRequest.java
+++ src/main/java/KanbanProject/web/api/BoardManipulationRequest.java
@@ -1,4 +1,42 @@
 package KanbanProject.web.api;
 
 public class BoardManipulationRequest {
+    private String board_name;
+    private String board_description;
+    private String board_owner;
+    private String board_status;
+
+    public BoardManipulationRequest() {}
+    public BoardManipulationRequest(String board_name, String board_description, String board_owner, String board_status) {
+        this.board_name = board_name;
+        this.board_description = board_description;
+        this.board_owner = board_owner;
+        this.board_status = board_status;
+    }
+    public String getBoard_name() {
+        return board_name;
+    }
+    public void setBoard_name(String board_name) {
+        this.board_name = board_name;
+    }
+    public String getBoard_description() {
+        return board_description;
+    }
+    public void setBoard_description(String board_description) {
+        this.board_description = board_description;
+    }
+    public String getBoard_owner() {
+        return board_owner;
+    }
+    public void setBoard_owner(String board_owner) {
+        this.board_owner = board_owner;
+    }
+    public String getBoard_status() {
+        return board_status;
+    }
+    public void setBoard_status(String board_status) {
+        this.board_status = board_status;
+    }
+
+
 }
--- src/main/java/KanbanProject/web/api/TaskManipulationRequest.java
+++ src/main/java/KanbanProject/web/api/TaskManipulationRequest.java
@@ -1,4 +1,49 @@
 package KanbanProject.web.api;
 
 public class TaskManipulationRequest {
+    private String task_name;
+    private String task_description;
+    private String task_owner;
+    private String task_status;
+    private Long board_id;
+
+    public TaskManipulationRequest() {}
+    public TaskManipulationRequest( String task_name, String task_description, String task_owner, String task_status, Long board_id) {
+        this.task_name = task_name;
+        this.task_description = task_description;
+        this.task_owner = task_owner;
+        this.task_status = task_status;
+        this.board_id = board_id;
+    }
+    public String getTask_name() {
+        return task_name;
+    }
+    public void setTask_name(String task_name) {
+        this.task_name = task_name;
+    }
+    public String getTask_description() {
+        return task_description;
+    }
+    public void setTask_description(String task_description) {
+        this.task_description = task_description;
+    }
+    public String getTask_owner() {
+        return task_owner;
+    }
+    public void setTask_owner(String task_owner) {
+        this.task_owner = task_owner;
+    }
+    public String getTask_status() {
+        return task_status;
+    }
+    public void setTask_status(String task_status) {
+        this.task_status = task_status;
+    }
+    public Long getBoard_id() {
+        return board_id;
+    }
+    public void setBoard_id(Long board_id) {
+        this.board_id = board_id;
+    }
+
 }
