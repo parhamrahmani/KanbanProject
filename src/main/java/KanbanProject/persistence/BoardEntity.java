32ff4e422d297ca68085cf13b6c1b0adf3530f57
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-14 16:23:15 +0100

    Entities made and developed

--- src/main/java/KanbanProject/persistence/BoardEntity.java
+++ src/main/java/KanbanProject/persistence/BoardEntity.java
@@ -1,4 +1,73 @@
 package KanbanProject.persistence;
 
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity (name = "board")
 public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "board_id", nullable = false)
+    private long board_id;
+
+    @Column(name = "board_name", nullable = false)
+    private String board_name;
+
+    @Column(name = "board_description", nullable = false)
+    private String board_description;
+
+    @Column(name = "board_owner", nullable = false)
+    private String board_owner;
+
+    @OneToMany(mappedBy = "board", fetch = FetchType.EAGER)
+    private final List<TaskEntity> taskEntities = new ArrayList<>();
+
+    public BoardEntity(String name, String description, String owner) {
+        this.board_name = name;
+        this.board_description = description;
+        this.board_owner = owner;
+    }
+
+protected BoardEntity() {}
+
+    public long getBoard_id() {
+        return board_id;
+    }
+
+    public void setBoard_id(long board_id) {
+        this.board_id = board_id;
+    }
+
+    public String getBoard_name() {
+        return board_name;
+    }
+
+    public void setBoard_name(String name) {
+        this.board_name = name;
+    }
+
+    public String getBoard_description() {
+        return board_description;
+    }
+
+    public void setBoard_description(String description) {
+        this.board_description = description;
+    }
+
+    public String getBoard_owner() {
+        return board_owner;
+    }
+
+    public void setBoard_owner(String owner) {
+        this.board_owner = owner;
+    }
+
+    public List<TaskEntity> getTaskEntities() {
+        return taskEntities;
+    }
+
+
+
 }
--- src/main/java/KanbanProject/persistence/TaskEntity.java
+++ src/main/java/KanbanProject/persistence/TaskEntity.java
@@ -1,4 +1,81 @@
 package KanbanProject.persistence;
 
+import javax.persistence.*;
+
+@Entity (name = "task")
 public class TaskEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id", nullable = false)
+    private Long task_id;
+
+    @Column(name = "task_name", nullable = false)
+    private String task_name;
+
+    @Column(name = "task_description", nullable = false)
+    private String task_description;
+
+    @Column(name = "task_status", nullable = false)
+    private String task_status;
+
+    @Column(name = "owner", nullable = false)
+    private String task_owner;
+
+    @ManyToOne(cascade = CascadeType.MERGE, targetEntity = BoardEntity.class)
+    private BoardEntity board;
+
+    protected TaskEntity() {}
+
+    public TaskEntity(String name, String description, String status, String owner, BoardEntity board) {
+        this.task_name = name;
+        this.task_description = description;
+        this.task_status = status;
+        this.task_owner = owner;
+        this.board = board;
+    }
+
+    public String getTask_name() {
+        return task_name;
+    }
+
+    public void setTask_name(String name) {
+        this.task_name = name;
+    }
+
+    public String getTask_description() {
+        return task_description;
+    }
+
+    public void setTask_description(String description) {
+        this.task_description = description;
+    }
+
+    public String getTask_status() {
+        return task_status;
+    }
+
+    public void setTask_status(String status) {
+        this.task_status = status;
+    }
+
+    public String getTask_owner() {
+        return task_owner;
+    }
+    public void setTask_owner(String owner) {
+        this.task_owner = owner;
+    }
+    public Long getTask_id() {
+        return task_id;
+    }
+
+    public void setTask_id(Long task_id) {
+        this.task_id = task_id;
+    }
+    public BoardEntity getBoard() {
+        return board;
+    }
+    public void setBoard(BoardEntity board) {
+        this.board = board;
+    }
 }
