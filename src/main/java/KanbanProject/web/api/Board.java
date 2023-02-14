3fe2da3e907ecdda9d213fe200d9b1d47e9da51b
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-14 16:25:08 +0100

    Objects implemented

--- src/main/java/KanbanProject/web/api/Board.java
+++ src/main/java/KanbanProject/web/api/Board.java
@@ -1,4 +1,59 @@
 package KanbanProject.web.api;
 
+import java.util.List;
+
 public class Board {
+    private Long board_id;
+    private String board_name;
+    private String board_description;
+    private String board_owner;
+    private List<Task> tasks;
+
+  public Board(Long board_id, String board_name, String board_description, String board_owner, List<Task> tasks) {
+        this.board_id = board_id;
+        this.board_name = board_name;
+        this.board_description = board_description;
+        this.board_owner = board_owner;
+        this.tasks = tasks;
+    }
+    public Long getBoard_id() {
+        return board_id;
+    }
+
+    public void setBoard_id(Long board_id) {
+        this.board_id = board_id;
+    }
+
+    public String getBoard_name() {
+        return board_name;
+    }
+
+    public void setBoard_name(String board_name) {
+        this.board_name = board_name;
+    }
+
+    public String getBoard_description() {
+        return board_description;
+    }
+
+    public void setBoard_description(String board_description) {
+        this.board_description = board_description;
+    }
+
+    public String getBoard_owner() {
+        return board_owner;
+    }
+
+    public void setBoard_owner(String board_owner) {
+        this.board_owner = board_owner;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
+
 }
