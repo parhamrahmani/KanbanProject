fd89228671f59893065504a1eae4997d7cc04723
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-14 16:24:07 +0100

    Repositories extended by JPARepsotiory

--- src/main/java/KanbanProject/persistence/BoardRepository.java
+++ src/main/java/KanbanProject/persistence/BoardRepository.java
@@ -1,4 +1,14 @@
 package KanbanProject.persistence;
 
-public class BoardRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+    List<BoardEntity> findAll();
+    List<BoardEntity> findAllByBoardName(String boardName);
+    List<BoardEntity> findAllByOwner(String owner);
+    List<BoardEntity> findBoardEntitiesByBoard_id(Long board_id);
 }
--- src/main/java/KanbanProject/persistence/TaskRepository.java
+++ src/main/java/KanbanProject/persistence/TaskRepository.java
@@ -1,4 +1,16 @@
 package KanbanProject.persistence;
 
-public interface TaskRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<TaskEntity, Long> {
+    List<TaskEntity> findAll();
+    List<TaskEntity> findAllByTaskName(String taskName);
+    List<TaskEntity> findAllByOwner(String owner);
+    List<TaskEntity> findTaskEntitiesByTask_id(Long task_id);
+    List<TaskEntity> findTaskEntitiesByBoard(BoardEntity board);
+    List<TaskEntity> findTaskEntitiesByStatus(String status);
 }
