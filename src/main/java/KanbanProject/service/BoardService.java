49cd7aa6468ff1ae0cf8d601f9efaadd642dace2
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-15 16:20:33 +0100

    BoardService implemented
    findAll
    findById
    createTask
    editTask
    deleteTask

--- src/main/java/KanbanProject/service/BoardService.java
+++ src/main/java/KanbanProject/service/BoardService.java
@@ -1,4 +1,39 @@
 package KanbanProject.service;
 
+import KanbanProject.persistence.BoardEntity;
+import KanbanProject.persistence.BoardRepository;
+import KanbanProject.web.api.Board;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class BoardService {
+private final BoardRepository boardRepository;
+    private final BoardTransformer boardTransformer;
+
+    public BoardService(BoardRepository boardRepository, BoardTransformer boardTransformer) {
+        this.boardRepository = boardRepository;
+        this.boardTransformer = boardTransformer;
+    }
+
+    public List<Board> findAll(){
+        List<BoardEntity> boardEntities = boardRepository.findAll();
+        return boardEntities.stream().map(boardTransformer::boardTransformer).toList();
+    }
+
+    public Board findById(long id){
+        var boardEntity = boardRepository.findById(id);
+        return boardEntity.stream().map(boardTransformer::boardTransformer).findFirst().orElse(null);
+    }
+
+    public Board createBoard(Board board){
+        var boardEntity = new BoardEntity(board.getBoard_name(),board.getBoard_description(),board.getBoard_owner());
+        boardEntity = boardRepository.save(boardEntity);
+        return boardTransformer.boardTransformer(boardEntity);
+    }
+
+
 }
+
+
