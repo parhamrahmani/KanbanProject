71133e9b8c7c29990a054a05f5d18d763ca31e24
Parham Rahmani <parhamrahmani13762@gmail.com>
2023-02-15 16:21:05 +0100

    BoardTransformer implemented

--- src/main/java/KanbanProject/service/BoardTransformer.java
+++ src/main/java/KanbanProject/service/BoardTransformer.java
@@ -1,4 +1,23 @@
 package KanbanProject.service;
 
+import KanbanProject.persistence.BoardEntity;
+import KanbanProject.persistence.TaskEntity;
+import KanbanProject.web.api.Board;
+import org.springframework.stereotype.Service;
+
+import java.util.stream.Collectors;
+
+@Service
 public class BoardTransformer {
+    public Board boardTransformer(BoardEntity boardEntity)
+    {
+        var tasks = boardEntity.getTaskEntities().stream().map(TaskEntity::getId).toList();
+        return new Board(
+                boardEntity.getBoard_id(),
+                boardEntity.getBoard_name(),
+                boardEntity.getBoard_description(),
+                boardEntity.getBoard_owner(),
+                tasks
+        );
+    };
 }
